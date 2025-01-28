43d3efcdee1f8fc5cadb17c8f46960cc551921df
A-Mounika29 <129580671+A-Mounika29@users.noreply.github.com>
2025-01-28 09:17:34 +0530

    Update ProjectsStyle.js

--- src/components/Projects/ProjectsStyle.js
+++ src/components/Projects/ProjectsStyle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import _default from '../../themes/default';
 
 export const Container = styled.div`
-    background: linear-gradient(343.07deg, rgb(67, 50, 67) 5.71%, rgb(68, 49, 65) 64.83%);
+    background: linear-gradient(343.07deg, rgb(111, 122, 119) 5.71%, rgb(113, 160, 160) 64.83%);
     display: flex;
     flex-direction: column;
     justify-content: center;
