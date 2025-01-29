226d59fec1e1bcba1b332bafcb28b368a0b08371
A-Mounika29 <abburimounika0@gmail.com>
2025-01-29 12:31:03 +0530

    Updates

---
+++ precache-manifest.09a9153a973f4a2370c2c55c231e9701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a028250a5893b024c30786a2767dbf",
+    "url": "/portfolios/index.html"
+  },
+  {
+    "revision": "e9aa12c474d47c50898c",
+    "url": "/portfolios/static/css/main.628b5629.chunk.css"
+  },
+  {
+    "revision": "116388eabc1615d15a9a",
+    "url": "/portfolios/static/js/2.1fcd39ab.chunk.js"
+  },
+  {
+    "revision": "e9aa12c474d47c50898c",
+    "url": "/portfolios/static/js/main.125b281e.chunk.js"
+  },
+  {
+    "revision": "01f8aac3878c0853ec1f",
+    "url": "/portfolios/static/js/runtime~main.0928a3c9.js"
+  }
+]);
